3d5340985a715160b3a4816ac0bfa864bdda7835
SakaiTaka23 <tt3.sakai@gmail.com>
2024-04-27 11:09:38 +0900

    pass stage 1

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,38 @@
-fun main() {
-    println("Hello, World!")
+import io.ktor.network.selector.SelectorManager
+import io.ktor.network.sockets.aSocket
+import io.ktor.network.sockets.openReadChannel
+import io.ktor.network.sockets.openWriteChannel
+import io.ktor.utils.io.readUTF8Line
+import io.ktor.utils.io.writeStringUtf8
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+
+private const val REDIS_PORT = 6379
+
+public suspend fun main() {
+    runBlocking {
+        val selectorManager = SelectorManager(Dispatchers.IO)
+        val serverSocket = aSocket(selectorManager).tcp().bind("127.0.0.1", REDIS_PORT)
+        println("Server started at ${serverSocket.localAddress}")
+
+        while (true) {
+            val socket = serverSocket.accept()
+            println("Accepted $socket")
+            launch {
+                val receiveChannel = socket.openReadChannel()
+                val sendChanel = socket.openWriteChannel(autoFlush = true)
+                sendChanel.writeStringUtf8("Please enter your name\n")
+                try {
+                    while (true) {
+                        val name = receiveChannel.readUTF8Line()
+                        sendChanel.writeStringUtf8("Hello, $name\n")
+                    }
+                } catch (e: Throwable) {
+                    println("Connection lost $e")
+                    socket.close()
+                }
+            }
+        }
+    }
 }
