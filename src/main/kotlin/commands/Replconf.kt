76b8aef4b197348138334c4903ce85e56095f371
SakaiTaka23 <tt3.sakai@gmail.com>
2024-05-04 10:38:07 +0900

    implement replconf:ack command

--- src/main/kotlin/commands/Replconf.kt
+++ src/main/kotlin/commands/Replconf.kt
@@ -5,3 +5,13 @@ import resp.Protocol
 public class Replconf : CommandRoutes {
     override fun run(protocol: Protocol): Protocol = Protocol(mutableListOf("OK"))
 }
+
+public class ReplconfAck : CommandRoutes {
+    override fun run(protocol: Protocol): Protocol {
+        if (protocol.arguments[1] == "GETACK") {
+            return Protocol(mutableListOf("REPLCONF", "ACK", "0"))
+        } else {
+            error("invalid command")
+        }
+    }
+}
--- src/test/kotlin/commands/ReplconfTest.kt
+++ src/test/kotlin/commands/ReplconfTest.kt
@@ -3,6 +3,7 @@ package commands
 import resp.Protocol
 import kotlin.test.Test
 import kotlin.test.assertEquals
+import kotlin.test.assertFailsWith
 
 public class ReplconfTest {
     @Test
@@ -11,5 +12,27 @@ public class ReplconfTest {
         val result = Replconf().run(protocol)
 
         assertEquals(Protocol(mutableListOf("OK")), result)
+    }
+}
+
+public class ReplconfAckTest {
+    @Test
+    public fun `returns ack on request`() {
+        val protocol = Protocol(mutableListOf("REPLCONF", "GETACK", "*"))
+        val result = ReplconfAck().run(protocol)
+
+        assertEquals(Protocol(mutableListOf("REPLCONF", "ACK", "0")), result)
+    }
+
+    @Test
+    public fun `returns error on invalid command`() {
+        val protocol = Protocol(mutableListOf("REPLCONF", "GETAC", "some"))
+        val replconfAck = ReplconfAck()
+
+        val exception = assertFailsWith<IllegalStateException> {
+            replconfAck.run(protocol)
+        }
+
+        assertEquals("invalid command", exception.message)
     }
 }
