18ed9d79e6da4514442a7b59247f1c6e58966501
SakaiTaka23 <tt3.sakai@gmail.com>
2024-05-15 10:02:18 +0900

    changed spawn command

--- spawn_redis_server.sh
+++ spawn_redis_server.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 set -e
-mvn -B --quiet package -Ddir=/tmp/codecrafters-redis-target
-exec java -jar /tmp/codecrafters-redis-target/java_redis.jar "$@"
+pwd
+exec java -jar ../codecrafters-redis-kotlin/target/java_redis.jar "$@"
