45b1251e6ac342eae856a916e726d192826f297c
SakaiTaka23 <tt3.sakai@gmail.com>
2024-05-19 15:17:20 +0900

    refactored xadd to remove code used multiple times

--- src/main/kotlin/commands/Xadd.kt
+++ src/main/kotlin/commands/Xadd.kt
@@ -8,16 +8,21 @@ private const val COUNT_TO_KEY_VALUE = 3
 public class Xadd(private val repo: StreamStorage) : CommandRoutes {
     override fun run(protocol: Protocol): Protocol {
         val streamKey = protocol.arguments[1]
-        val timeStamp = protocol.arguments[2]
+        val rawTimeStamp = protocol.arguments[2]
+        val timeStamp = rawTimeStamp.splitTimeStamp()
         val keyValue = mutableMapOf<String, String>()
         val arguments = protocol.arguments.drop(COUNT_TO_KEY_VALUE)
 
-        if (!validateMinimum(timeStamp)) {
+        val milliSeconds = timeStamp[0].toIntOrNull() ?: return Protocol(mutableListOf())
+        val sequenceNumber = timeStamp[1].toIntOrNull() ?: return Protocol(mutableListOf())
+        val latestTimeStamp = repo.latestTimeStamp(streamKey).splitTimeStamp()
+
+        if (!validateMinimum(milliSeconds, sequenceNumber)) {
             return Protocol(
                 mutableListOf("ERR The ID specified in XADD must be greater than 0-0"),
             )
         }
-        if (!validateTimestamp(streamKey, timeStamp)) {
+        if (!validateTimestamp(milliSeconds, sequenceNumber, latestTimeStamp)) {
             return Protocol(
                 mutableListOf("ERR The ID specified in XADD is equal or smaller than the target stream top item"),
             )
@@ -28,31 +33,24 @@ public class Xadd(private val repo: StreamStorage) : CommandRoutes {
                 keyValue[arguments[i]] = arguments[i + 1]
             }
         }
-        repo.set(streamKey, timeStamp, keyValue.toMap())
+        repo.set(streamKey, rawTimeStamp, keyValue.toMap())
 
-        return Protocol(mutableListOf(timeStamp))
+        return Protocol(mutableListOf(rawTimeStamp))
     }
 
-    private fun validateMinimum(rawTimeString: String): Boolean {
-        val input = rawTimeString.splitTimeStamp()
-        val milliSeconds = input[0].toIntOrNull() ?: return false
-        val sequenceNumber = input[1].toIntOrNull() ?: return false
-        return !(milliSeconds == 0 && sequenceNumber == 0)
-    }
+    private fun validateMinimum(milliSeconds: Int, sequenceNumber: Int): Boolean =
+        !(milliSeconds == 0 && sequenceNumber == 0)
 
-    private fun validateTimestamp(streamKey: String, rawTimeString: String): Boolean {
-        val latest = repo.latestTimeStamp(streamKey).splitTimeStamp()
-        val input = rawTimeString.splitTimeStamp()
-        val milliSeconds = input[0].toIntOrNull() ?: return false
-        val sequenceNumber = input[1].toIntOrNull() ?: return false
-
-        return if (latest[0].toInt() > milliSeconds) {
-            false
-        } else if (latest[0].toInt() == milliSeconds) {
-            latest[1].toInt() < sequenceNumber
-        } else {
-            true
-        }
+    private fun validateTimestamp(
+        milliSeconds: Int,
+        sequenceNumber: Int,
+        latestTimeStamp: List<String>,
+    ): Boolean = if (latestTimeStamp[0].toInt() > milliSeconds) {
+        false
+    } else if (latestTimeStamp[0].toInt() == milliSeconds) {
+        latestTimeStamp[1].toInt() < sequenceNumber
+    } else {
+        true
     }
 }
 
