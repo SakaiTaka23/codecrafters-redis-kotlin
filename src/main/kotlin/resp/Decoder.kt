62676e4cdc456a031abc99d9e857d3fdf6116c9c
SakaiTaka23 <tt3.sakai@gmail.com>
2024-05-19 17:41:50 +0900

    change decode to return * when string is only *

--- src/main/kotlin/resp/Decoder.kt
+++ src/main/kotlin/resp/Decoder.kt
@@ -2,7 +2,12 @@ package resp
 
 public object Decoder {
     private fun simpleString(string: String): String = string.removePrefix("+").deleteCRCL()
-    private fun arrayCount(string: String): String = string.removePrefix("*").deleteCRCL()
+    private fun arrayCount(string: String): String = if (string == "*") {
+        "*"
+    } else {
+        string.removePrefix("*").deleteCRCL()
+    }
+
     private fun String.deleteCRCL(): String = this.removeSuffix("\r\n")
 
     public fun read(encoded: String): String {
